474ae6fb0584ebc9b3eeb111472dae16939a3d1f
Flukeza07x <mr.fluke2544@gmail.com>
2025-01-04 06:54:43 +0700

    Update database.ts

--- config/database.ts
+++ config/database.ts
@@ -2,11 +2,11 @@ export default ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
+      host: env('MYSQL_ADDON_HOST'),
+      port: env.int('MYSQL_ADDON_PORT'),
+      database: env('MYSQL_ADDON_DB'),
+      user: env('MYSQL_ADDON_USER'),
+      password: env('MYSQL_ADDON_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
